3e9b01da12852589c75086747371eaba057c91bc
Pavlo <plyj.v.nebo@gmail.com>
2021-03-15 18:11:36 +0200

    Init

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = () => (
+    <div>
+        <h1 className="bg-gray-100 p-2 mx-auto w-1/2 mt-2 rounded shadow">
+            Laravel
+        </h1>
+    </div>
+);
+
+export default Welcome;
