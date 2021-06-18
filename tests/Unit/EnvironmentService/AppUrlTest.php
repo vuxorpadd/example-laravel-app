43c40d9bd3f572169c34ba11779707fef298595b
Pavlo <plyj.v.nebo@gmail.com>
2021-06-18 15:43:36 +0300

    Fix broken test

--- tests/Unit/EnvironmentService/AppUrlTest.php
+++ tests/Unit/EnvironmentService/AppUrlTest.php
@@ -3,14 +3,28 @@
 namespace Tests\Unit\EnvironmentService;
 
 use App\EnvironmentService;
-use PHPUnit\Framework\TestCase;
+use Illuminate\Contracts\Console\Kernel;
+use Tests\TestCase;
 
 class AppUrlTest extends TestCase
 {
+    public function createApplication()
+    {
+        $app = require __DIR__ . "/../../../bootstrap/app.php";
+
+        $app->loadEnvironmentFrom("tests/Unit/EnvironmentService/.env.clear");
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
-        $_ENV = [];
+
+        putenv("LARAVEL_SAIL");
+        putenv("APP_URL");
+        putenv("APP_PORT");
     }
 
     /**
@@ -18,7 +32,7 @@ class AppUrlTest extends TestCase
      */
     public function getting_app_url_for_artisan_serve()
     {
-        $_ENV["APP_URL"] = "http://myurl:9000";
+        putenv("APP_URL=http://myurl:9000");
 
         $environmentService = app(EnvironmentService::class);
         $this->assertEquals("http://myurl:9000", $environmentService->appUrl());
@@ -41,7 +55,7 @@ class AppUrlTest extends TestCase
      */
     public function getting_default_app_url_for_sail()
     {
-        $_ENV["LARAVEL_SAIL"] = "1";
+        putenv("LARAVEL_SAIL=1");
         $environmentService = app(EnvironmentService::class);
         $this->assertEquals("http://localhost", $environmentService->appUrl());
     }
@@ -51,8 +65,8 @@ class AppUrlTest extends TestCase
      */
     public function getting_default_app_url_for_sail_with_port_specified()
     {
-        $_ENV["LARAVEL_SAIL"] = "1";
-        $_ENV["APP_PORT"] = "8001";
+        putenv("LARAVEL_SAIL=1");
+        putenv("APP_PORT=8001");
 
         $environmentService = app(EnvironmentService::class);
         $this->assertEquals(
@@ -66,8 +80,8 @@ class AppUrlTest extends TestCase
      */
     public function getting_app_url_for_sail()
     {
-        $_ENV["LARAVEL_SAIL"] = "1";
-        $_ENV["SAIL_APP_URL"] = "http://my_cutom_url:8001";
+        putenv("LARAVEL_SAIL=1");
+        putenv("SAIL_APP_URL=http://my_cutom_url:8001");
 
         $environmentService = app(EnvironmentService::class);
         $this->assertEquals(
@@ -81,9 +95,9 @@ class AppUrlTest extends TestCase
      */
     public function app_url_takes_precedence_over_app_port()
     {
-        $_ENV["LARAVEL_SAIL"] = "1";
-        $_ENV["APP_PORT"] = "8001";
-        $_ENV["SAIL_APP_URL"] = "http://my_cutom_url:8002";
+        putenv("LARAVEL_SAIL=1");
+        putenv("APP_PORT=8001");
+        putenv("SAIL_APP_URL=http://my_cutom_url:8002");
 
         $environmentService = app(EnvironmentService::class);
         $this->assertEquals(
